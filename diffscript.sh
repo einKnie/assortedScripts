bc9f99c7363e5b6b14f417bfad126a3913c2724d
lülü <einKnie@gmx.at>
2020-10-17 02:40:10 +0200

    added quiet mode and made more robust

--- diffscript.sh
+++ diffscript.sh
@@ -8,67 +8,91 @@
 #   $0 -x1 ./lbimaker_old -x2 ./lbimaker_new -c [ testcmds]
 
 print_help() {
-  echo "refactorTester v0.1"
-  echo
-  echo "usage:"
-  echo " $name -x1 <executable1> -x2 <executable2> -c [commands ... ]"
-  echo 
-  echo "note: commands will glob all remaining arguments"
-  echo "      so it should be the last argument given"
-  echo
+  log "refactorTester v0.1"
+  log
+  log "usage:"
+  log " $scriptname -x1 <executable1> -x2 <executable2> -c [commands \"...\" [-q -h ]"
+  log 
+  log " -x1\t... left-side executable"
+  log " -x2\t... right-side executable"
+  log "  -c\t... commands (must be quoted)"
+  log "  -q\t... quiet mode, disables all output to stdout"
+  log "  -h\t... show this help screen"
 }
 
-name="$(basename $0)"
+log() {
+  if [ "$quiet" -eq 0 ] ; then
+    echo -e "$1"
+  fi
+}
+
+scriptname="$(basename "$0")"
 path="$(cd "$(dirname "$0")"; pwd -P)/"
 bin_left=""
 bin_right=""
 res_left="$path/res_left"
 res_right="$path/res_right"
+quiet=0
 
-echo "running in \"$path\""
+# find '-q' first to turn off logging from the start
+case "$@" in
+  *-q*)
+    quiet=1
+    ;;
+esac
+
+log "running in \"$path\""
 
 while [ "$#" -ne 0 ]; do
   case $1 in
     -x1) 
       shift
       bin_left="$1"
-      echo -e "left:      $bin_left"
+      log "left:      $bin_left"
       ;;
     -x2)
       shift
       bin_right="$1"
-      echo -e "right:     $bin_right"
+      log "right:     $bin_right"
       ;;
     -c)
       shift
-      command="$@"
-      echo -e "commands:  \"$command\""
+      command="$1"
+      log "commands:  \"$command\""
+      ;;
+    -q)
+      ;;
+    -h | --help)
+      quiet=0
+      print_help
+      exit 0
       ;;
     *)
       print_help
-      exit 1
+      exit -1
   esac
   shift
 done
 
-echo
+log ""
 
 err=0
-[ -x "$bin_left"  ] || (echo "executable 1 is not actually executable!" ; err=1)
-[ -x "$bin_right" ] || (echo "executable 2 is not actually exacutable!" ; err=1)
-[ "$err" -eq 1    ] && exit 1
+[[ -d "$bin_left"  || ! -x "$bin_left"  ]] && { log "executable 1 is not actually an executable!" ; err=1 ; }
+[[ -d "$bin_right" || ! -x "$bin_right" ]] && { log "executable 2 is not actually an executable!" ; err=1 ; }
+ 
+[ "$err" -eq 1 ] && exit -1
 
-$($bin_left $command > $res_left)
-$($bin_right $command > $res_right)
+"$bin_left" $command > "$res_left"
+"$bin_right" $command > "$res_right"
 
-res="$(diff $res_left $res_right)"
+res="$(diff "$res_left" "$res_right")"
 if [ "$?" -eq 0 ]; then
-  echo "it's the same!"
+  log "it's the same!"
   exit 0
 else
-  echo "there are differencs!"
-  echo
-  echo -e "$res"
+  log "there are differencs!"
+  log
+  log "$res"
   exit 1
 fi
 
