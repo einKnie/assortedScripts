119403d1c414f41d2d026ee7e2366e100dfdcad1
einKnie <einKnie@gmx.at>
2020-10-21 16:35:35 +0200

    fixed diffscript executable execution

--- diffscript.sh
+++ diffscript.sh
@@ -12,7 +12,7 @@ print_help() {
   log
   log "usage:"
   log " $scriptname -x1 <executable1> -x2 <executable2> -c [commands \"...\" [-q -h ]"
-  log 
+  log
   log " -x1\t... left-side executable"
   log " -x2\t... right-side executable"
   log "  -c\t... commands (must be quoted)"
@@ -45,7 +45,7 @@ log "running in \"$path\""
 
 while [ "$#" -ne 0 ]; do
   case $1 in
-    -x1) 
+    -x1)
       shift
       bin_left="$1"
       log "left:      $bin_left"
@@ -58,7 +58,7 @@ while [ "$#" -ne 0 ]; do
     -c)
       shift
       command="$1"
-      log "commands:  \"$command\""
+      log "commands:  $command"
       ;;
     -q)
       ;;
@@ -74,16 +74,16 @@ while [ "$#" -ne 0 ]; do
   shift
 done
 
-log ""
+log
 
 err=0
 [[ -d "$bin_left"  || ! -x "$bin_left"  ]] && { log "executable 1 is not actually an executable!" ; err=1 ; }
 [[ -d "$bin_right" || ! -x "$bin_right" ]] && { log "executable 2 is not actually an executable!" ; err=1 ; }
- 
+
 [ "$err" -eq 1 ] && exit -1
 
-"$bin_left" $command > "$res_left"
-"$bin_right" $command > "$res_right"
+eval "$bin_left" ${command} > "$res_left"
+eval "$bin_right" ${command} > "$res_right"
 
 res="$(diff "$res_left" "$res_right")"
 if [ "$?" -eq 0 ]; then
@@ -95,4 +95,3 @@ else
   log "$res"
   exit 1
 fi
-
